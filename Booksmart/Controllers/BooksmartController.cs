818de81f654d6b912f1e9b104c47724886574dfb
Nhlanhla <u18324887@tuks.co.za>
2019-10-05 21:24:25 +0200

    quiz

--- Booksmart/Controllers/BooksmartController.cs
+++ Booksmart/Controllers/BooksmartController.cs
@@ -37,7 +37,16 @@ namespace Booksmart.Controllers
         }
         public ActionResult ABCquiz()
         {
-            return View();
+            Del_4_272Entities db = new Del_4_272Entities();
+            db.Configuration.ProxyCreationEnabled = false;
+            //Random rnd = new Random();
+            //load questions to view
+            var qlist = db.TheoryQuestions.Where(xx => xx.Type == 1).ToList();
+            //var questions = db.TheoryQuestions.Where(xx => xx.Type == 0).OrderBy().ToList();
+            var questions = from item in qlist
+                            orderby Guid.NewGuid() ascending
+                            select item;
+            return View(questions.ToList());
         }
         public ActionResult ABCsong()
         {
